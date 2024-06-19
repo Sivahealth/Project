f717ecb1416d50cc1411a68a82610b4cf8c0bb90
Thushitha1910 <thushitha1910@gmail.com>
2024-06-19 16:48:21 +0530

    Finish Add Appointment Page

--- project/src/Components/Add_appointment/Add_appointment.jsx
+++ project/src/Components/Add_appointment/Add_appointment.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import '../Sign_up/SignUpForm.css'
 import './Add_appointment.css'
 import '../Logmenu/WhiteRec.css'
 import logo from '../Images/logo.png';
@@ -11,6 +10,82 @@ function Add_appointment () {
     <div className='Add_appointment_logo_Container'>
     <img src={logo} alt="Logo" className="appointment_logo"/>
     <p className='Add_appointment_Optimize-text'>Siva Health Hub</p>
+    </div>
+    <div className='appointment_text_container'>
+      <p className='appointment-title'>Your appointment schedule</p>
+    <form>
+      <div className="input-group2">
+            <label htmlFor="text" className="label2">Patient's Name</label>
+            <input type="text1" id="username" name="username" placeholder='First Name'required />
+      </div>
+      <div className="input-group3">
+             <input type="text2" id="username" name="username" placeholder='Last Name'required />
+      </div>
+       <div className="dob-group">
+       <label htmlFor="text" className="label2">Date of Birth</label>
+       <input type="text1" id="username" name="username" placeholder='Date'required />
+       </div>
+
+       <div className="dob-group1">
+       <input type="text2" id="username" name="username" placeholder='Month'required />
+       </div>
+       
+       <div className="dob-group2">
+       <input type="text2" id="username" name="username" placeholder='Year'required />
+       </div>
+
+       <div className="gender-group">
+       <label htmlFor="text1" className="label2">Gender</label>
+       <select className="select1" id="gender" name="gender" required>
+              <option value="" disabled selected></option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+       </div>
+
+      <div className="age-group">
+      <label htmlFor="text1" className="label2">Age</label>
+      <input type="text1" id="username" name="username" required />
+      </div>
+     
+     <div className="mail-group">
+     <label htmlFor="text1" className="label3">E-mail</label>
+     <input type="text1" id="username" name="username" placeholder='myname@example.com'required />
+     </div>
+
+    <div className="address-group">
+    <label htmlFor="text1" className="label3">Address</label>
+    <input type="text1" id="username" name="username" required />
+    </div>
+
+    <div className="city-group">
+    <input type="text2" id="username" name="username" placeholder='City'required />
+    </div>
+
+    <div className="province-group">
+    <input type="text2" id="username" name="username" placeholder='Province'required />
+    </div>
+
+    <div className='select-depatment'>
+    <input type="text2" id="username" name="username" placeholder='Select Department'required />
+    </div>
+
+    <div className='contact-group'>
+    <input type="text2" id="username" name="username" placeholder='Contact Number'required />
+    </div>
+
+      </form>
+    <div className="label4">
+      Which department would you like to  get appointment from ?
+    </div>
+    </div>
+
+    <div className='label5'>
+      Appointment date and time, <a href="#">click here</a>.
+    </div>
+
+    <div className='button-container'>
+    <button type="submit" className="submit-button">Book an Appointment</button>
     </div>
 
     </div>
