1b375ef22c4c7563325b3926abd9cc885a63498a
Thushitha1910 <thushitha1910@gmail.com>
2024-07-01 23:08:52 +0530

    Connect backend with front end login

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,11 @@
 
 import User from "../models/user.js";
 import { hashPassword } from "../helpers/auth.js";
+import { comparePassword } from "../helpers/auth.js";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const register = async (req, res) => {
   try {
@@ -42,6 +47,37 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   // Function implementation
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Username and password are required' });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(400).json({ error: 'Incorrect password' });
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+
+    res.json({
+      user: {
+        username: user.email,
+      },
+      token,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
 };
 
 export const secret = async (req, res) => {
