e487a1d97cc89d184da89cc4938158aebb57d301
Vyshnnavi-P <157089507+Vyshnnavi-P@users.noreply.github.com>
2024-07-10 13:33:23 +0530

    Update doctorRoutes.js
    error handling enhanced

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -1,14 +1,14 @@
-// routes/doctorRoutes.js
 import express from 'express';
-import { addDoctor, getDoctors, updateDoctor, deleteDoctor,getAvailableSlots,getDoctorsByDate } from '../controllers/doctorController.js';
+import { addDoctor, getDoctors, updateDoctor, deleteDoctor, getAvailableSlots, getDoctorsByDate } from '../controllers/doctorController.js';
+import { requireSignin, isAdmin } from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.post('/doctors', addDoctor);
+router.post('/doctors', requireSignin, isAdmin, addDoctor);
 router.get('/doctors', getDoctors);
-router.put('/doctors/:id', updateDoctor);
-router.delete('/doctors/:id', deleteDoctor);
+router.put('/doctors/:id', requireSignin, isAdmin, updateDoctor);
+router.delete('/doctors/:id', requireSignin, isAdmin, deleteDoctor);
 router.get('/doctors/:doctorId/slots', getAvailableSlots);
-/*router.get('/doctors', getDoctorsByDate);*/
-router.post('/by-date', getDoctorsByDate);
+router.post('/doctors/by-date', getDoctorsByDate);
+
 export default router;
